c953926eabbdcd6d361faa42bed0030281768f2d
adamsyy <adamoommenjacob@gmail.com>
2022-06-17 13:24:01 +0530

    shared prefs2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:seventy5/Home.dart';
 import 'package:seventy5/username.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +15,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late String username;
+  late String password;
+  late String token;
   // This widget is the root of your application.
   @override
   void initState() {
     // TODO: implement initState
-
+get_token();
+getcred();
   }
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(home: Username(),);
   }
+getcred()async{
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  setState(() {
+    username = prefs.getString('username')!;
+    password = prefs.getString('password')!;
+
+  });
+
+}
+  get_token()async{
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      token = prefs.getString('token')!;
+
+
+    });
+
+  }
+
 }
 
